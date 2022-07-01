71497bcb693a3f107a3e2ad86d43ec09d43e2cc7
AliAhmedKhan <alistpaulscr7@gmail.com>
2022-07-01 13:07:04 +0500

    Latest update. Modal component rendering on every state change (solved) modal component rendering and state changing simultaneously (not solved)

--- src/components/UserInfo.js
+++ src/components/UserInfo.js
@@ -4,7 +4,6 @@ import { useNavigate } from "react-router-dom";
 import availableflightAPI from "../availableflightAPI";
 import Tofrom from "./Tofrom";
 import TravelerModal from "./TravelerModal";
-import { useEffect } from "react";
 
 const UserInfo = ({ setuserdata }) => {
   const navigate = useNavigate();
